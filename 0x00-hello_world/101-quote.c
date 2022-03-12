f974a85e83802d1f4044d914ea135155a45f7e0d
waregagbagbo <gabelwarega@gmail.com>
2022-03-12 18:26:31 +0300

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,2 @@
-#include <stdio.h>
-/*
- *
- *
- *
- *
- */
-int main(){
-
-	scanf('and that piece of art is useful" - Dora Korpar, 2015-10-19\n')
-		
-		return 1
-
-
-
-}
+#!/bin/bash
+gcc -S -masm=intel $CFILE
