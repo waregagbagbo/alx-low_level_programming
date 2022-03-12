bbe0c2edff1267cdda5756ca855ab0c0f235aab7
waregagbagbo <gabelwarega@gmail.com>
2022-03-12 18:32:59 +0300

    file 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
+int main(void)
+{
+	int s;
+
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
+}
