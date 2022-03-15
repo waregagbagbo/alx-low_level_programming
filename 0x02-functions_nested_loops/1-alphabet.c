37ea7633e674bee1dcf2da8ac5fe257ae5d5a652
waregagbagbo <gabelwarega@gmail.com>
2022-03-16 02:09:57 +0300

    file1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ 
+ *
+ */
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+   
+   return(0);
+	}
