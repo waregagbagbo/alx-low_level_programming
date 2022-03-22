eeeddfd2e0aa35b92a2a471a75035ad26b08cb42
Mill_Man <44092807+waregagbagbo@users.noreply.github.com>
2022-03-22 17:15:50 +0300

    Create 3-puts.c
    file 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *str){
+  
+  str = 'My house of wax is very amazing to buy';
+  _puts(str);
+  return(0);
+}
