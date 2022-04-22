c050966d2b3430a057afceb5049c06f9c2e2f79c
Mill_Man <44092807+waregagbagbo@users.noreply.github.com>
2022-04-22 15:26:49 +0300

    Create 4-free_list.c
    file 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *a;
+
+	while (head)
+	{
+		a = head->next;
+		free(head->str);
+		free(head);
+		head = a;
+	}
+}
